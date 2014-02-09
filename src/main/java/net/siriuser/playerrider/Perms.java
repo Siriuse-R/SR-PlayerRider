ab7fb587676b7e9351a9430dbddb6f37b0eef320
Siriuse-R <admin@dejiplus.net>
2014-02-10 03:06:59 +0900

    [Add] Permissions Manager

---
+++ src/main/java/net/siriuser/playerrider/Perms.java
@@ -0,0 +1,24 @@
+package net.siriuser.playerrider;
+
+import org.bukkit.permissions.Permissible;
+
+public enum Perms {
+    DEBUG ("debug"),
+    ;
+
+    final String HEADER = "playerrider.";
+    private String node;
+
+    Perms(final String node) {
+        this.node = HEADER + node;
+    }
+    public boolean has(final Permissible perm) {
+        if (perm == null)
+            return false;
+        return perm.hasPermission(node); // only support SuperPerms
+    }
+
+    public String getNode() {
+        return this.node;
+    }
+}
